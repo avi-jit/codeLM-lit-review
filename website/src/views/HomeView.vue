17ba78d18e93d0428bdcfc6c9bf2320769e4f5f7
Avijit Thawani <avijit.thawani@gmail.com>
2023-01-22 10:33:07 -0800

    Initial commit

---
+++ website/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import constants from "../../constants.json";
+document.title = `${constants["website-title"]} | Home`;
+</script>
+
+<template>
+  <div>
+    <h1>{{ constants["home-title"] }}</h1>
+    <br />
+    <p>{{ constants["home-description"] }}</p>
+  </div>
+</template>
